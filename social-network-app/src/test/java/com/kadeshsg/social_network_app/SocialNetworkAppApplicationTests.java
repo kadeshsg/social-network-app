26cf4434070f2ff5e9a739a1a8dc152072d9cd2b
kadeshsg <kadeshgs@gmail.com>
2025-09-25 15:10:36 +0530

    Initial Spring Boot project setup

---
+++ social-network-app/src/test/java/com/kadeshsg/social_network_app/SocialNetworkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadeshsg.social_network_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
